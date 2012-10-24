ff131b682eec938fa13e845f8abead2e4f31009f
Artur <lis314372@yandex.ru>
2012-10-24 15:57:15 +0400

    1111

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Site::Application.initialize!
 
-Site::Application.configure do
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-	:port => 587,
-  :user_name  => "lyapinartur@gmail.com",
-  :password  => "lis314372",
-  :authentication  => :login
-}
-end
