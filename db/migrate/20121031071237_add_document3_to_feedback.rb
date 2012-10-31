c88fbd46fb1490695190541cae07c4aaa12f7acb
Artur <lis314372@yandex.ru>
2012-10-31 12:36:02 +0400

    delete migration2

--- db/migrate/20121031071237_add_document3_to_feedback.rb
+++
@@ -1,5 +0,0 @@
-class AddDocument3ToFeedback < ActiveRecord::Migration
-  def change
-    add_column :feedbacks, :document3, :string
-  end
-end
--- db/migrate/20121031080241_add_document2_to_feedback.rb
+++
@@ -1,5 +0,0 @@
-class AddDocument2ToFeedback < ActiveRecord::Migration
-  def change
-    add_column :feedbacks, :document2, :file
-  end
-end
