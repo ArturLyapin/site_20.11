423543392e5d1d6a04d6d3edf2e0c575fb8e639f
Artur <lis314372@yandex.ru>
2012-10-31 12:41:12 +0400

    delete migration3

--- db/migrate/20121031082525_remove_document3_from_feedback.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDocument3FromFeedback < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20121031082547_remove_document2_from_feedback.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDocument2FromFeedback < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
