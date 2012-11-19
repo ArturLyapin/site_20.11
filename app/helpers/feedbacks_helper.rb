e74d44d841d9fd4648ce8161979d3362f6a1ff23
Artur <lis314372@yandex.ru>
2012-11-19 17:55:29 +0400

    site with rails admin

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -1,15 +1,6 @@
 module FeedbacksHelper
 def list_class(link_title)
-<<<<<<< HEAD
-	@action=params[:action]
-	if @action==link_title
-		return " class=selected"
-	else
-		return
-	end		
-		
-=======
+
 	  controller.action_name == link_title ? " class=selected" : ""
->>>>>>> ac552fa482dc1261f5c2d40ca170ac3a5bdb2e02
 end
 end
