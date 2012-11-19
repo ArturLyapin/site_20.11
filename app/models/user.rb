fe0a5e75097b1321c1934e296e162842f00bd059
Artur <lis314372@yandex.ru>
2012-11-19 17:48:08 +0400

    site with rails admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -9,7 +9,4 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-=======
-  attr_accessible :name, :password_digest
->>>>>>> ac552fa482dc1261f5c2d40ca170ac3a5bdb2e02
 end
